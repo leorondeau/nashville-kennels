7e136d13239c5be9b919c8c45517ddf99fc6325f
admin <christopherleorondeau@gmail.com>
2020-11-23 11:31:42 -0600

    completed all animal dom rendering

--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -1,5 +1,7 @@
-import React from "react"   
-import "./Kennel.css"
+import React from "react"
+import { Animal } from "./animal/Animal"
+import "./animal/Animal.css"
+
 
 export const Kennel = () => (
     <>
@@ -9,5 +11,12 @@ export const Kennel = () => (
         <div>Visit Us at the Nashville North Location</div>
         <div>500 Puppy Way</div>
     </address>
+
+    <h2>Animals</h2>
+    <article className="animals">
+        <Animal />
+        <Animal />
+        <Animal />
+    </article>
     </>
 )
