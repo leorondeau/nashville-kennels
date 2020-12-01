73a207809d7ff2fe640c5106564fb4d699739f27
Leo Rondeau <christopherleorondeau@gmail.com>
2020-12-01 14:25:30 -0600

    location feature added that has link to location details

--- src/components/animal/Animal.js
+++ src/components/animal/Animal.js
@@ -1,11 +1,14 @@
 import React from "react"
+import { Link } from "react-router-dom"
 import "./Animal.css"
 
-export const Animal = ({ animal , location , customer }) => (
+export const Animal = ({ animal }) => (
     <section className="animal">
-        <h3 className="animal__name">{animal.name}</h3>
+        <h3 className="animal__name">
+            <Link to={`/animals/${animal.id}`}>
+                { animal.name }
+            </Link>
+        </h3>
         <div className="animal__breed">{animal.breed}</div>
-        <div className="animal__location">Location: { location.name }</div>
-        <div className="animal__owner">Customer: {customer.name }</div>
     </section>
 )
--- src/components/animal/AnimalProvider.js
+++ src/components/animal/AnimalProvider.js
@@ -11,6 +11,11 @@ export const AnimalProvider = (props) => {
             .then(setAnimals)
     }
 
+    const getAnimalById = (id) => {
+        return fetch ('http://localhost:8088/animals/${ id }?_expand=location&_expand=customer')
+            .then(res => res.json())
+    }
+
     const addAnimal = animal => {
         return fetch("http://localhost:8088/animals" , {
             method: "POST",
@@ -24,7 +29,7 @@ export const AnimalProvider = (props) => {
 
     return (
         <AnimalContext.Provider value={{
-            animals , addAnimal , getAnimals
+            animals , addAnimal , getAnimals , getAnimalById
         }}>
             {props.children}
         </AnimalContext.Provider>
